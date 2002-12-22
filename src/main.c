6a9274351e1fb085339393814707c070baa3b376
Sam Hocevar <sam@hocevar.net>
2002-12-22 18:42:32 +0000

    * added the COPYING file (GPLv2).   * added GPL headers to source files.

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
  *   Copyright (c) 2002 Sam Hocevar <sam@zoy.org>
  *                 All Rights Reserved
  *
- *   $Id tarass
+ *   $Id: tarass
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
