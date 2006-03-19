e7deb17101cb04512f54f69b96187876cb09d302
Sam Hocevar <sam@hocevar.net>
2006-03-19 16:59:34 +0000

    * Added a lame "powered by libcaca" banner to cacaplas.

--- src/cacaplas.c
+++ src/cacaplas.c
@@ -114,6 +114,9 @@ paused:
         cucul_draw_bitmap(qq, 0, 0,
                           cucul_get_width(qq) - 1, cucul_get_height(qq) - 1,
                           bitmap, screen);
+        cucul_set_color(qq, CUCUL_COLOR_WHITE, CUCUL_COLOR_BLUE);
+        cucul_putstr(qq, cucul_get_width(qq) - 30, cucul_get_height(qq) - 2,
+                     " -=[ Powered by libcaca ]=- ");
         caca_display(kk);
     }
 
