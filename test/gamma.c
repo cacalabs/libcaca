fabd8c746737f2052d243eecc834eea6efa8cec7
Sam Hocevar <sam@hocevar.net>
2006-03-22 15:00:43 +0000

    * Added colour to the gamma test.

--- test/gamma.c
+++ test/gamma.c
@@ -24,7 +24,7 @@ typedef unsigned int uint32_t;
 #include "cucul.h"
 #include "caca.h"
 
-uint32_t buffer[256];
+uint32_t buffer[256 * 4];
 
 int main(void)
 {
@@ -38,11 +38,16 @@ int main(void)
     kk = caca_attach(qq);
 
     for(x = 0; x < 256; x++)
+    {
         buffer[x] = (x << 16) | (x << 8) | (x<< 0);
+        buffer[x + 256] = (0xff << 16) |    (x << 8) | (0x00 << 0);
+        buffer[x + 512] = (0x00 << 16) | (0xff << 8) |    (x << 0);
+        buffer[x + 768] =    (x << 16) | (0x00 << 8) | (0xff << 0);
+    }
 
-    left = cucul_create_bitmap(qq, 32, 256, 1, 4 * 256,
+    left = cucul_create_bitmap(qq, 32, 256, 4, 4 * 256,
                                0x00ff0000, 0x0000ff00, 0x000000ff, 0x0);
-    right = cucul_create_bitmap(qq, 32, 256, 1, 4 * 256,
+    right = cucul_create_bitmap(qq, 32, 256, 4, 4 * 256,
                                 0x00ff0000, 0x0000ff00, 0x000000ff, 0x0);
     caca_set_delay(kk, 20000);
 
@@ -63,7 +68,7 @@ int main(void)
                           right, buffer);
 
         cucul_set_color(qq, CUCUL_COLOR_WHITE, CUCUL_COLOR_BLUE);
-        cucul_printf(qq, 1, 2, "gamma %g", g);
+        cucul_printf(qq, 2, 1, "gamma=%g", g);
 
         caca_display(kk);
     }
