959778330885f73745658c3fc09062fad8defc04
Sam Hocevar <sam@hocevar.net>
2006-12-12 01:53:54 +0000

    * Bwarf, typo in the no warranty clause.

--- test/colors.c
+++ test/colors.c
@@ -5,7 +5,7 @@
  *
  *  $Id$
  *
- *  This program is free software. It commes without any warranty, to
+ *  This program is free software. It comes without any warranty, to
  *  the extent permitted by applicable law. You can redistribute it
  *  and/or modify it under the terms of the Do What The Fuck You Want
  *  To Public License, Version 2, as published by Sam Hocevar. See
