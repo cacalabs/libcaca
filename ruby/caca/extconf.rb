92414f5af0a470b0f880b12dfae001e250bcbfa1
Pascal Terjan <pterjan@linuxfr.org>
2007-11-15 12:27:40 +0000

    * Use autotools for Ruby bindings

--- ruby/caca/extconf.rb
+++
@@ -1,5 +0,0 @@
-require 'mkmf'
-
-pkg_config("caca")
-$CFLAGS += " -Wall"
-create_makefile("caca")
--- ruby/cucul/extconf.rb
+++
@@ -1,5 +0,0 @@
-require 'mkmf'
-
-pkg_config("cucul")
-$CFLAGS += " -Wall"
-create_makefile("cucul")
