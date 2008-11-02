2bb297d690b397a68662190002a9bcec01077f6a
Ben Wiley Sittler <bsittler@gmail.com>
2008-11-02 20:29:02 +0000

    htmlification of truecolor.php; evidently there are some bugs yet in the html3 exporter

--- caca-php/examples/www/truecolor.php
+++ caca-php/examples/www/truecolor.php
@@ -1,4 +1,10 @@
-#!/usr/bin/php5
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <?php
 /*
  *  truecolor	  truecolor canvas features
@@ -17,15 +23,17 @@
  *  To Public License, Version 2, as published by Sam Hocevar. See
  *  http://sam.zoy.org/wtfpl/COPYING for more details.
  */
+?>
+<head>
+<title>truecolor canvas features</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body text="silver" bgcolor="black">
+<?php
 
 $cv = caca_create_canvas(32, 16);
 if(!$cv) {
 	die("Failed to create canvas\n");
-}
-
-$dp = caca_create_display($cv);
-if(!$dp) {
-	die("Failed to create display\n");
 }
 
 for($y = 0; $y < 16; $y++)
@@ -41,8 +49,8 @@ for($y = 0; $y < 16; $y++)
 caca_set_color_ansi($cv, CACA_WHITE, CACA_LIGHTBLUE);
 caca_put_str($cv, 2, 1, " truecolor libcaca ");
 
-caca_refresh_display($dp);
-
-caca_get_event($dp, CACA_EVENT_KEY_PRESS, -1);
+echo caca_export_string($cv, "html3");
 
 ?>
+</body>
+</html>
