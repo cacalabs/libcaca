4b01269d8a888121c790803974970edc798331c0
Ben Wiley Sittler <bsittler@gmail.com>
2008-11-04 00:56:07 +0000

    only fill the preview iframe when javascript is enabled

--- caca-php/examples/www/export.php
+++ caca-php/examples/www/export.php
@@ -98,7 +98,7 @@ $export_php = isset($_SERVER['SCRIPT_NAME'])
 		$_SERVER['SCRIPT_NAME']
 		:
 		'export.php';
-?><iframe name="preview" id="preview" src="<?= htmlspecialchars($export_php) ?>?format=html" width="820" height="620" style="margin: 0; padding: 0; border: none; width: 100%"></iframe>
+?><iframe name="preview" id="preview" width="820" height="620" style="margin: 0; padding: 0; border: none; width: 100%"></iframe>
 </body>
 </html>
 <?php
