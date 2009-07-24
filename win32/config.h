33d2bc209284cff0837f1ff53c331fc7b501c9f8
Sam Hocevar <sam@hocevar.net>
2009-07-24 16:39:43 +0000

    Update the Win32 and Win64 ports.

--- win32/config.h
+++ win32/config.h
@@ -22,7 +22,7 @@
 #define HAVE_SLEEP 1
 /* #undef HAVE_STDINT_H */
 #define HAVE_STDLIB_H 1
-/* #undef HAVE_STRCASECMP */
+#define HAVE_STRCASECMP 1
 #define HAVE_STRINGS_H 1
 #define HAVE_STRING_H 1
 /* #undef HAVE_SYS_IOCTL_H */
