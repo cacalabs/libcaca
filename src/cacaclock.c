8c758dd1a405b42cff2928f4ec601b199143e08a
Jean-Yves Lamoureux <jylam@lnxscene.org>
2011-03-06 08:18:46 +0000

    * Removed unused variable

--- src/cacaclock.c
+++ src/cacaclock.c
@@ -25,8 +25,6 @@
 
 static void usage(int argc, char **argv)
 {
-	char const * const * list;
-
 	fprintf(stderr, "Usage: %s [OPTIONS]... <IMAGE>\n", argv[0]);
 	fprintf(stderr, "Convert IMAGE to any text based available format.\n");
 	fprintf(stderr, "Example : %s -w 80 -f ansi ./caca.png\n\n", argv[0]);
